0f92ad00cc4f8055836fe4c7c1549a08be765ac3
Bipon Roy <biponroy400@gmail.com>
2023-11-05 01:45:56 +0600

    updated error page

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,16 +1,19 @@
 import { Link } from "react-router-dom";
-
+import errorImage from "../../assets/Error.jpg";
 const Error = () => {
     return (
-        <div>
-            <div className="h-[70vh]">
-                <div className="text-center pt-40 space-y-5">
+        <div className="">
+            <div>
+                {/* <div className="text-center pt-40 space-y-5">
                     <span className="text-9xl text-red-600">404!</span>
                     <h1 className="text-5xl">Nothings Found Here.</h1>
+                </div> */}
+                <div className="flex justify-center">
+                    <img className="md:h-[700px]" src={errorImage} alt="" />
                 </div>
                 <div className="flex justify-center mt-8">
-                    <Link className="px-5 py-2 bg-red-600 font-semibold text-white rounded">
-                        Go Back
+                    <Link to="/" className="px-5 py-2 bg-red-600 font-semibold text-white rounded">
+                        Back to home
                     </Link>
                 </div>
             </div>
