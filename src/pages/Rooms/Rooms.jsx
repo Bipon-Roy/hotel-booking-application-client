a33966ceaa9383a1db351c410689dfecf0203181
Bipon Roy <biponroy400@gmail.com>
2023-11-08 22:13:20 +0600

    Added new functionality on filtering rooms

--- src/pages/Rooms/Rooms.jsx
+++ src/pages/Rooms/Rooms.jsx
@@ -8,13 +8,16 @@ import { Helmet } from "react-helmet-async";
 const Rooms = () => {
     const axiosURl = useAxiosUrl();
     const [rooms, setRooms] = useState([]);
-    const [price, setPrice] = useState("");
-
+    const [sortOrder, setSortOrder] = useState("");
+    const [minPrice, setMinPrice] = useState("");
+    const [maxPrice, setMaxPrice] = useState("");
     useEffect(() => {
-        axiosURl.get(`/rooms?sortField=price_per_night&sortOrder=${price}`).then((res) => {
-            setRooms(res.data);
-        });
-    }, [axiosURl, price]);
+        axiosURl
+            .get(`/rooms?minPrice=${minPrice}&maxPrice=${maxPrice}&sortOrder=${sortOrder}`)
+            .then((res) => {
+                setRooms(res.data);
+            });
+    }, [axiosURl, minPrice, maxPrice, sortOrder]);
 
     return (
         <div className="max-w-7xl mx-auto">
@@ -24,7 +27,7 @@ const Rooms = () => {
             <h1 className="text-center font-semibold text-3xl my-8">
                 Our Hotel <span className="text-primary">Rooms</span>
             </h1>
-            <div className="flex justify-between items-center border p-3 mb-6 mx-4 md:mx-8 lg:mx-0">
+            <div className="flex flex-col lg:flex-row justify-between items-center border p-3 mb-6 mx-4 md:mx-8 lg:mx-0">
                 <div>
                     <Link
                         onClick={() => document.getElementById("my_modal_3").showModal()}
@@ -33,11 +36,24 @@ const Rooms = () => {
                         Leave A Review
                     </Link>
                 </div>
-                <div className="flex items-center gap-1 md:gap-4">
+                <div className="flex flex-col lg:flex-row items-center gap-1 md:gap-4">
                     <p className="font-medium text-sm md:text-base">Sort By Price</p>
+
+                    <input
+                        className="border py-1 pl-3 w-[150px] focus:outline-none"
+                        type="number"
+                        placeholder="Min Price"
+                        onChange={(e) => setMinPrice(e.target.value)}
+                    />
+                    <input
+                        className="border py-1 pl-3 w-[150px] focus:outline-none"
+                        type="number"
+                        placeholder="Max Price"
+                        onChange={(e) => setMaxPrice(e.target.value)}
+                    />
                     <select
                         className="md:px-5 py-2 border focus:outline-none"
-                        onChange={(e) => setPrice(e.target.value)}
+                        onChange={(e) => setSortOrder(e.target.value)}
                     >
                         <option value="">Chose One</option>
                         <option value="desc">From High To Low</option>
