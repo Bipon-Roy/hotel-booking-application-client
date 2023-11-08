5073a8c36031919e0bcc076ef58c4a7248f8c495
Bipon Roy <biponroy400@gmail.com>
2023-11-08 15:51:08 +0600

    Added Room Review Counts functionality on Rooms page

--- src/pages/Rooms/Cards.jsx
+++ src/pages/Rooms/Cards.jsx
@@ -1,8 +1,24 @@
 import { BsFillPeopleFill } from "react-icons/bs";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import useAxiosUrl from "../../Hook/useAxiosUrl";
 const Cards = ({ card }) => {
     const { _id, room_thumbnail, title, room_description, price_per_night, max_occupancy } = card;
+    const [reviews, setReviews] = useState([]);
+
+    const axiosURl = useAxiosUrl();
+    useEffect(() => {
+        axiosURl
+            .get(`/reviews/${_id}`)
+            .then((res) => {
+                setReviews(res.data);
+            })
+            .catch((e) => console.log(e));
+    }, [axiosURl, _id]);
+
+    console.log(reviews);
+
     return (
         <div className="flex flex-col bg-base-100 shadow-xl transition-all hover:scale-105">
             <Link to={`/roomDetails/${_id}`}>
@@ -10,17 +26,28 @@ const Cards = ({ card }) => {
                     <img className="h-[200px] w-full" src={room_thumbnail} alt={title} />
                 </figure>
             </Link>
-            <div className="p-4 flex-grow space-y-2">
+            <div className=" flex-grow space-y-2 px-4 py-2">
                 <h2 className="font-bold text-lg">{title}</h2>
                 <p>{room_description}</p>
+                {reviews.length === 0 ? (
+                    <p className="text-primary font-medium">No reviews Available</p>
+                ) : (
+                    <p className="font-bold text-primary flex gap-1 items-center">
+                        <span className="text-textMain">Reviews:</span> {reviews.length}
+                        <BsFillPeopleFill className="text-primary text-lg" />
+                    </p>
+                )}
             </div>
-            <div className="flex justify-between p-4">
+            <div className="flex justify-between px-4 pb-3">
                 <p className="font-bold text-primary">
                     <span className="text-textMain">Price:</span> {price_per_night}$ / Night
                 </p>
+
                 <div className="flex items-center gap-1 mr-2">
-                    <p className="font-bold">{max_occupancy}</p>
-                    <BsFillPeopleFill className="text-primary text-lg" />
+                    <p className="font-bold">Allowed: {max_occupancy}</p>
+                    <p>
+                        <BsFillPeopleFill className="text-primary text-lg" />
+                    </p>
                 </div>
             </div>
         </div>
