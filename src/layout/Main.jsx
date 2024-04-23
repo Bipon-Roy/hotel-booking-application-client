7309ed31f802a19c7ec01ab2309da18ddebabae5
Bipon Roy <biponroy400@gmail.com>
2024-04-23 16:41:12 +0600

    fixed overflow issues on small screen

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -17,7 +17,7 @@ const Main = () => {
         });
     }, [location.pathname]);
     return (
-        <div className="flex flex-col min-h-screen overflow-hiddenfir">
+        <div className="flex flex-col min-h-screen overflow-hidden">
             <div className="flex-none">
                 <Navbar />
             </div>
